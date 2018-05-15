0ea18643ab9347336e9eaefd8cab435d143a0d93
yurezzz13 <yurkadurka@gmail.com>
2018-05-15 14:48:11 +0300

    JDBC.class adding try-catch

--- src/main/java/database/JDBC.java
+++ src/main/java/database/JDBC.java
@@ -9,15 +9,38 @@ public class JDBC {
     public static final String USERNAME = "postgres";
     public static final String PASSWORD = "postgres";
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    public static void main(String[] args){
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         Statement statement =
-                connection.createStatement();
-        statement.execute("CREATE TABLE hero(id INT PRIMARY KEY NOT NULL," +
-                "name TEXT NOT NULL, raceName TEXT NOT NULL, professionName " +
-                "TEXT NOT NULL, health DOUBLE PRECISION NOT NULL, " +
-                "manaPoint DOUBLE PRECISION NOT NULL, " +
-                "ragePoint DOUBLE PRECISION NOT NULL);");
+                null;
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            statement.execute("CREATE TABLE hero(id INT PRIMARY KEY NOT NULL," +
+                    "name TEXT NOT NULL, raceName TEXT NOT NULL, professionName " +
+                    "TEXT NOT NULL, health DOUBLE PRECISION NOT NULL, " +
+                    "manaPoint DOUBLE PRECISION NOT NULL, " +
+                    "ragePoint DOUBLE PRECISION NOT NULL);");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
